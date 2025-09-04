8342bfaadf4e9cf7681cd5b1d6d981bf9a29c986
Wassim Madwar <wassimmadwar@gmail.com>
2025-09-04 19:27:10 +0200

    test Loader

--- js/script.js
+++ js/script.js
@@ -1,27 +1,49 @@
 function animateLogo() {
-    const logoSlide = document.getElementById('logo-slide');
-    const svgLogo = logoSlide.querySelector('svg');
-    const logoPaths = svgLogo.querySelectorAll('path');
+  const logoSlide = document.getElementById("logo-slide");
+  const svgLogo = logoSlide.querySelector("svg");
+  const logoPaths = svgLogo.querySelectorAll("path");
 
-    // Startposition: zentriert, Originalgröße (statisch in CSS)
-    logoPaths.forEach(path => path.setAttribute('fill', 'white'));
+  // Startposition: zentriert, Originalgröße (statisch in CSS)
+  logoPaths.forEach((path) => path.setAttribute("fill", "white"));
+
+  setTimeout(() => {
+    // Animation: Logo verschiebt sich nach links oben und schrumpft
+    svgLogo.style.transition =
+      "width 1s cubic-bezier(.77,0,.18,1), height 1s cubic-bezier(.77,0,.18,1), left 1s cubic-bezier(.77,0,.18,1), top 1s cubic-bezier(.77,0,.18,1)";
+    svgLogo.style.width = "100px";
+    svgLogo.style.height = (100 * 324) / 265 + "px"; // Ratio beibehalten
+    logoPaths.forEach((path) => path.setAttribute("fill", "#4589FF"));
+    logoSlide.style.background = "transparent";
 
     setTimeout(() => {
-        // Animation: Logo verschiebt sich nach links oben und schrumpft
-        svgLogo.style.transition = 'width 1s cubic-bezier(.77,0,.18,1), height 1s cubic-bezier(.77,0,.18,1), left 1s cubic-bezier(.77,0,.18,1), top 1s cubic-bezier(.77,0,.18,1)';
-        svgLogo.style.width = '100px';
-        svgLogo.style.height = (100 * 324 / 265) + 'px'; // Ratio beibehalten
-        logoPaths.forEach(path => path.setAttribute('fill', '#4589FF'));
-        logoSlide.style.background = 'transparent';
+      // Endposition: SVG absolut positionieren, Abstand 77px/80px, Logo bleibt klein und links oben
+      svgLogo.style.position = "absolute";
+      svgLogo.style.left = "77px";
+      svgLogo.style.top = "80px";
+      svgLogo.style.width = "100px";
+      svgLogo.style.height = (100 * 324) / 265 + "px";
+      logoPaths.forEach((path) => path.setAttribute("fill", "#4589FF"));
+    }, 1000);
+  }, 500);
+}
+function Loadingscreen() {
+  if (window.innerWidth <= 600) {
+    loader.innerHTML = getLoadingscreenMobile();
+  } else {
+    loader.innerHTML = getLoadingscreenDesktop();
+  }
+}
 
-        setTimeout(() => {
-            // Endposition: SVG absolut positionieren, Abstand 77px/80px, Logo bleibt klein und links oben
-            svgLogo.style.position = 'absolute';
-            svgLogo.style.left = '77px';
-            svgLogo.style.top = '80px';
-            svgLogo.style.width = '100px';
-            svgLogo.style.height = (100 * 324 / 265) + 'px';
-            logoPaths.forEach(path => path.setAttribute('fill', '#4589FF'));
-        }, 1000);
-    }, 500);
+
+function getLoadingscreenMobile() {
+  return `
+    <img src="../assets/img/logo-black.svg" alt="Logo" id="loader-image-black" class="loader-image login-logo" />
+  `;
+}
+
+
+function getLoadingscreenDesktop() {
+  return `
+    <img src="../assets/img/logo-black.svg" alt="Logo" id="loader-image-black" class="loader-image" />
+  `;
 }
